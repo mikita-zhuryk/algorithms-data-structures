753471ab487d81c5e3eee2e84e928a602c71df48
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2019-02-19 10:30:43 +0300

    Added a simplified tree to 0.1

--- 0.1/Source.cpp
+++ 0.1/Source.cpp
@@ -1,16 +1,68 @@
 #include <fstream>
 #include <list>
-#include "../Data Structures/BinarySearchTree.h"
+
+struct TreeNode {
+
+	int* key;
+	TreeNode* left;
+	TreeNode* right;
+
+	TreeNode() {
+		key = nullptr;
+		left = nullptr;
+		right = nullptr;
+	}
+
+	TreeNode(int& key) {
+		this->key = new int(key);
+		left = nullptr;
+		right = nullptr;
+	}
+
+	void add(int& key) {
+		if (this->key == nullptr) {
+			this->key = new int(key);
+		}
+		else {
+			if (key < *(this->key)) {
+				if (left != nullptr) {
+					left->add(key);
+				}
+				else {
+					left = new TreeNode(key);
+				}
+			}
+			else if (key > *(this->key)) {
+				if (right != nullptr) {
+					right->add(key);
+				}
+				else {
+					right = new TreeNode(key);
+				}
+			}
+		}
+	}
+
+	void straightTraverseLeft(std::ofstream& out) {
+		out << *key << std::endl;
+		if (left != nullptr) {
+			left->straightTraverseLeft(out);
+		}
+		if (right != nullptr) {
+			right->straightTraverseLeft(out);
+		}
+	}
+
+};
 
 int main() {
 	int temp;
 	std::ifstream in("input.txt");
 	std::ofstream out("output.txt");
-	data_structures::BinarySearchTree<int> tree;
+	TreeNode tree;
 	while (in >> temp) {
 		tree.add(temp);
 	}
-	std::function<void(int&)> f = [&out](int key) { out << key << "\n"; };
-	tree.straightTraverseLeft(f);
+	tree.straightTraverseLeft(out);
 	return 0;
 }
