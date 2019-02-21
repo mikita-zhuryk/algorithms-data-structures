31b5f05e3a3db6d93cc22cf8c58060115a5b650e
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2019-02-21 20:41:42 +0300

    Removed main from Randomizer

--- Randomizer/Source.cpp
+++ Randomizer/Source.cpp
@@ -8,14 +8,10 @@ void fillTXT(std::string path, std::size_t n) {
 	std::ofstream out(path);
 	std::vector<int> numbers;
 	for (size_t i = 0; i < n; ++i) {
-		numbers.push_back(rand());
+		numbers.push_back(rand() % 1000);
 	}
 	out << numbers[rand() % numbers.size()] << "\n" << "\n";
 	for (size_t i = 0; i < n; ++i) {
 		out << numbers[i] << "\n";
 	}
-}
-
-int main() {
-	fillTXT("../0.2/input.txt", 5000);
 }
