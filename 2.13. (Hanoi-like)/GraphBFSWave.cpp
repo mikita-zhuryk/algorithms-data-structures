8a108f59897cc75d4d1395ddaa3e4cee8c2ab698
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2019-04-05 11:15:52 +0300

    Implemented BFS for transfusions

--- 2.13. (Hanoi-like)/GraphBFSWave.cpp
+++ 2.13. (Hanoi-like)/GraphBFSWave.cpp
@@ -1,109 +1,120 @@
-//#include <fstream>
-//#include <iostream>
-//#include <vector>
-//#include <algorithm>
-//#include <queue>
-//#pragma comment(linker, "/STACK:67108864")
-//#define SUM_VOLUME 100
-//#define MATRIX_DIM SUM_VOLUME + 1
-//
-//using namespace std;
-//
-//typedef uint32_t vertex;
-//
-//struct State {
-//
-//	short first;
-//	short second;
-//	short third;
-//
-//	State() {
-//		first = second = third = 0;
-//	}
-//
-//	State(short f, short s, short t) {
-//		first = f;
-//		second = s;
-//		third = t;
-//	}
-//
-//	bool operator==(const State& s) const {
-//		return (first == s.first) && (second == s.second) && (third == s.third);
-//	}
-//
-//};
-//
-//vector<pair<State, vector<State>>> buildAdjacencyList(State& s, vector<short>& measures) {
-//	vector<pair<State, vector<State>>> adj(1);
-//	bool used;
-//	short measure;
-//	queue<State> q;
-//	State temp;
-//	size_t i = 0;
-//	q.push(s);
-//	while (!q.empty()) {
-//		used = false;
-//		temp = q.front();
-//		q.pop();
-//		for (int i = 0; i < adj.size(); ++i) {
-//			if (temp == adj[i].first) {
-//				used = true;
-//				break;
-//			}
-//		}
-//		if (!used) {
-//			adj.push_back(make_pair(temp, vector<State>()));
-//			++i;
-//			for (int m = measures.size() - 1; m > 0; --m) {
-//				measure = measures[m];
-//
-//				if (temp.second + temp.first <= SUM_VOLUME) adj[i].second.push_back(State(0, temp.second + temp.first, temp.third));
-//				if (temp.third + temp.first <= SUM_VOLUME) adj[i].second.push_back(State(0, temp.second, temp.third + temp.first));
-//				if (temp.first + temp.second <= SUM_VOLUME) adj[i].second.push_back(State(temp.first + temp.second, 0, temp.third));
-//				if (temp.third + temp.second <= SUM_VOLUME) adj[i].second.push_back(State(temp.first, 0, temp.third + temp.second));
-//				if (temp.first + temp.third <= SUM_VOLUME) adj[i].second.push_back(State(temp.first + temp.third, temp.second, 0));
-//				if (temp.second + temp.third <= SUM_VOLUME) adj[i].second.push_back(State(temp.first, temp.second + temp.third, 0));
-//
-//				if ((temp.first - (-temp.second + measure) >= 0) && (temp.first - (-temp.second + measure) <= 100) && (temp.second <= measure)) {
-//					adj[i].second.push_back(State(temp.first - (measure - temp.second), measure, temp.third));
-//				}
-//				if ((temp.second - (-temp.first + measure) >= 0) && (temp.second - (-temp.first + measure) <= 100) && (temp.first <= measure)) {
-//					adj[i].second.push_back(State(measure, temp.second - (measure - temp.first), temp.third));
-//				}
-//				if ((temp.third - (-temp.first + measure) >= 0) && (temp.third - (-temp.first + measure) <= 100) && (temp.first <= measure)) {
-//					adj[i].second.push_back(State(measure, temp.second, temp.third - (measure - temp.first)));
-//				}
-//				if ((temp.third - (-temp.second + measure) >= 0) && (temp.third - (-temp.second + measure) <= 100) && (temp.second <= measure)) {
-//					adj[i].second.push_back(State(temp.first, measure, temp.third - (measure - temp.second)));
-//				}
-//			}
-//			for (size_t j = 0; j < adj[i].second.size(); ++j) {
-//				q.push(adj[i].second[j]);
-//			}
-//		}
-//	}
-//	cout << i << endl;
-//	return adj;
-//}
-//
-//int main() {
-//	ios_base::sync_with_stdio(false);
-//	ofstream m("temp1.txt");
-//	for (int i = 1; i < 100; ++i) {
-//		m << i << ' ';
-//	}
-//	short x;
-//	ifstream in("in.txt");
-//	ofstream out("out.txt");
-//	State init;
-//	in >> x >> init.first >> init.second;
-//	init.third = 100 - init.first - init.second;
-//	vector<short> measures;
-//	short temp;
-//	while (in >> temp) {
-//		measures.push_back(temp);
-//	}
-//	buildAdjacencyList(init, measures);
-//	system("pause");
-//	return 0;
-//}
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <iostream>
+#pragma comment(linker, "/STACK:67108864")
+#define SUM_VOLUME 100
+
+using namespace std;
+
+typedef uint32_t vertex;
+
+struct State {
+
+	short first;
+	short second;
+	short third;
+	uint32_t actions;
+
+	State() {
+		first = second = third = 0;
+		actions = UINT32_MAX;
+	}
+
+	State(short f, short s, short t, uint32_t ac) {
+		first = f;
+		second = s;
+		third = t;
+		actions = ac;
+	}
+
+};
+
+uint32_t hashState(State& s) {
+	return s.first * pow(SUM_VOLUME, 2) + s.second * SUM_VOLUME + s.third;
+}
+
+uint32_t waveBFS(State& s, vector<short>& measures, short x) {
+	short measure;
+	queue<State> q;
+	size_t size = pow(SUM_VOLUME, 3) + 1;
+	bool* visited = new bool[size];
+	for (size_t i = 0; i < size; ++i) {
+		visited[i] = false;
+	}
+	State temp;
+	size_t i = 0;
+	q.push(s);
+	while (!q.empty()) {
+		temp = q.front();
+		q.pop();
+		if (!visited[hashState(temp)]) {
+			//cout << temp.first << ' ' << temp.second << ' ' << temp.third << ' ' << temp.actions << endl;
+			if (temp.third == x) {
+				delete[] visited;
+				return temp.actions;
+			}
+			q.push(State(0, temp.second + temp.first, temp.third, temp.actions + 1));
+			q.push(State(0, temp.second, temp.third + temp.first, temp.actions + 1));
+			q.push(State(temp.first + temp.second, 0, temp.third, temp.actions + 1));
+			q.push(State(temp.first, 0, temp.third + temp.second, temp.actions + 1));
+			q.push(State(temp.first + temp.third, temp.second, 0, temp.actions + 1));
+			q.push(State(temp.first, temp.second + temp.third, 0, temp.actions + 1));
+			for (int m = 0; m < measures.size(); ++m) {
+				measure = measures[m];
+
+				if (temp.first > measure) {
+					q.push(State(measure, temp.second + (temp.first - measure), temp.third, temp.actions + 1));
+					q.push(State(measure, temp.second, temp.third + (temp.first - measure), temp.actions + 1));
+				}
+				if (temp.second > measure) {
+					q.push(State(temp.first + (temp.second - measure), measure, temp.third, temp.actions + 1));
+					q.push(State(temp.first, measure, temp.third + (temp.second - measure), temp.actions + 1));
+				}
+
+				if ((temp.first - (measure - temp.second) > 0) && (temp.second < measure)) {
+					q.push(State(temp.first - (measure - temp.second), measure, temp.third, temp.actions + 1));
+				}
+				if ((temp.second - (measure - temp.first) > 0) && (temp.first < measure)) {
+					q.push(State(measure, temp.second - (measure - temp.first), temp.third, temp.actions + 1));
+				}
+				if ((temp.third - (measure - temp.first) > 0) && (temp.first < measure)) {
+					q.push(State(measure, temp.second, temp.third - (measure - temp.first), temp.actions + 1));
+				}
+				if ((temp.third - (measure - temp.second) > 0) && (temp.second < measure)) {
+					q.push(State(temp.first, measure, temp.third - (measure - temp.second), temp.actions + 1));
+				}
+			}
+			visited[hashState(temp)] = true;
+		}
+	}
+	//cout << temp.first << ' ' << temp.second << ' ' << temp.third << ' ' << temp.actions << endl;
+	delete[] visited;
+	return UINT32_MAX;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	short x;
+	ifstream in("in.txt");
+	ofstream out("out.txt");
+	State init;
+	in >> x >> init.first >> init.second;
+	init.third = 100 - init.first - init.second;
+	init.actions = 0;
+	vector<short> measures;
+	short temp;
+	while (in >> temp) {
+		measures.push_back(temp);
+	}
+	measures.pop_back();
+	uint32_t result = waveBFS(init, measures, x);
+	if (result == UINT32_MAX) {
+		out << "No solution\n";
+	}
+	else {
+		out << result << endl;
+	}
+	//system("pause");
+	return 0;
+}
--- 2.13. (Hanoi-like)/New.cpp
+++ 2.13. (Hanoi-like)/New.cpp
@@ -1,107 +1,111 @@
-#include <fstream>
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#pragma comment(linker, "/STACK:67108864")
-#define SUM_VOLUME 100
-#define MATRIX_DIM SUM_VOLUME + 1
-
-using namespace std;
-
-struct State {
-
-	short first;
-	short second;
-	short third;
-
-};
-
-uint32_t findMinActions(uint32_t*** stateActions, short x) {
-	uint32_t min = UINT32_MAX;
-	uint32_t cur;
-	ofstream temp("temp.txt");
-	for (uint16_t i = 0; i < MATRIX_DIM; ++i) {
-		for (uint16_t j = 0; j < MATRIX_DIM; ++j) {
-			cur = stateActions[i][j][x];
-			if (cur < min) {
-				min = cur;
-			}
-			temp << cur << ' ';
-		}
-		temp << endl;
-	}
-	return min;
-}
-
-uint32_t countActions(uint32_t*** stateActions, State& s, vector<short>& measures, uint16_t x) {
-	short measure;
-	int k = 0;
-	for (int l = 0; l < SUM_VOLUME; ++l) {
-		for (int m = measures.size() - 1; m >= 0; --m) {
-			measure = measures[m];
-			for (int i = 0; i < MATRIX_DIM; ++i) {
-				for (int j = 0; j < MATRIX_DIM; ++j) {
-					k = SUM_VOLUME - i - j;
-					if ((k >= 0) && (stateActions[i][j][k] != UINT32_MAX)) {
-						//перелить всё
-						if (j + i <= SUM_VOLUME) stateActions[0][j + i][k] = min(stateActions[0][j + i][k], stateActions[i][j][k] + 1);
-						if (k + i <= SUM_VOLUME) stateActions[0][j][k + i] = min(stateActions[0][j][k + i], stateActions[i][j][k] + 1);
-						if (i + j <= SUM_VOLUME) stateActions[i + j][0][k] = min(stateActions[i + j][0][k], stateActions[i][j][k] + 1);
-						if (k + j <= SUM_VOLUME) stateActions[i][0][k + j] = min(stateActions[i][0][k + j], stateActions[i][j][k] + 1);
-						if (i + k <= SUM_VOLUME) stateActions[i + k][j][0] = min(stateActions[i + k][j][0], stateActions[i][j][k] + 1);
-						if (j + k <= SUM_VOLUME) stateActions[i][j + k][0] = min(stateActions[i][j + k][0], stateActions[i][j][k] + 1);
-						//долить до деления
-						if ((i - (-j + measure) >= 0) && (i - (-j + measure) <= SUM_VOLUME) && (j <= measure)) {
-							stateActions[i - (-j + measure)][measure][k] = min(stateActions[i][j][k] + 1, stateActions[i - (-j + measure)][measure][k]);
-						}
-						if ((j - (-i + measure) >= 0) && (j - (-i + measure) <= SUM_VOLUME) && (i <= measure)) {
-							stateActions[measure][j - (-i + measure)][k] = min(stateActions[i][j][k] + 1, stateActions[measure][j - (-i + measure)][k]);
-						}
-						if ((k - (-i + measure) >= 0) && (k - (-i + measure) <= SUM_VOLUME) && (i <= measure)) {
-							stateActions[measure][j][k - (-i + measure)] = min(stateActions[i][j][k] + 1, stateActions[measure][j][k - (-i + measure)]);
-						}
-						if ((k - (-j + measure) >= 0) && (k - (-j + measure) <= SUM_VOLUME) && (j <= measure)) {
-							stateActions[i][measure][k - (-j + measure)] = min(stateActions[i][j][k] + 1, stateActions[i][measure][k - (-j + measure)]);
-						}
-					}
-				}
-			}
-		}
-	}
-	return findMinActions(stateActions, x);
-}
-
-int main() {
-	ios_base::sync_with_stdio(false);
-	short x;
-	ifstream in("in.txt");
-	ofstream out("out.txt");
-	State init;
-	in >> x >> init.first >> init.second;
-	init.third = SUM_VOLUME - init.first - init.second;
-	vector<short> measures;
-	short temp;
-	while (in >> temp) {
-		measures.push_back(temp);
-	}
-	uint32_t*** stateActions = new uint32_t**[MATRIX_DIM];
-	for (uint16_t i = 0; i < MATRIX_DIM; ++i) {
-		stateActions[i] = new uint32_t*[MATRIX_DIM];
-		for (uint16_t j = 0; j < MATRIX_DIM; ++j) {
-			stateActions[i][j] = new uint32_t[MATRIX_DIM];
-			for (uint16_t k = 0; k < MATRIX_DIM; ++k) {
-				stateActions[i][j][k] = UINT32_MAX;
-			}
-		}
-	}
-	stateActions[init.first][init.second][init.third] = 0;
-	measures.pop_back();
-	uint32_t result = countActions(stateActions, init, measures, x);
-	if (result < UINT32_MAX) {
-		out << result << endl;
-	}
-	else {
-		out << "No solution" << endl;
-	}
-	return 0;
-}
+//#include <fstream>
+//#include <vector>
+//#include <algorithm>
+////#include <iostream>
+////#include <chrono>
+//#pragma comment(linker, "/STACK:67108864")
+//#define SUM_VOLUME 100
+//#define MATRIX_DIM SUM_VOLUME + 1
+//
+//using namespace std;
+////using namespace std::chrono;
+//
+//struct State {
+//
+//	short first;
+//	short second;
+//	short third;
+//
+//};
+//
+//uint32_t findMinActions(uint32_t*** stateActions, short x) {
+//	uint32_t min = UINT32_MAX;
+//	uint32_t cur;
+//	for (uint16_t i = 0; i < MATRIX_DIM; ++i) {
+//		for (uint16_t j = 0; j < MATRIX_DIM; ++j) {
+//			cur = stateActions[i][j][x];
+//			if (cur < min) {
+//				min = cur;
+//			}
+//		}
+//	}
+//	return min;
+//}
+//
+//uint32_t countActions(uint32_t*** stateActions, State& s, vector<short>& measures, uint16_t x) {
+//	short measure;
+//	uint32_t addition;
+//	int k = 0;
+//	for (int m = measures.size() - 1; m >= 0; --m) {
+//		measure = measures[m];
+//		for (int l = 0; l < MATRIX_DIM / measure; ++l) {
+//			for (int i = 0; i < MATRIX_DIM; ++i) {
+//				for (int j = 0; j < MATRIX_DIM - i; ++j) {
+//					k = SUM_VOLUME - i - j;
+//					if ((k >= 0) && (stateActions[i][j][k] != UINT32_MAX)) {
+//						//перелить всё
+//						addition = stateActions[i][j][k] + 1;
+//						if (j + i <= SUM_VOLUME) stateActions[0][j + i][k] = min(stateActions[0][j + i][k], addition);
+//						if (k + i <= SUM_VOLUME) stateActions[0][j][k + i] = min(stateActions[0][j][k + i], addition);
+//						if (i + j <= SUM_VOLUME) stateActions[i + j][0][k] = min(stateActions[i + j][0][k], addition);
+//						if (k + j <= SUM_VOLUME) stateActions[i][0][k + j] = min(stateActions[i][0][k + j], addition);
+//						if (i + k <= SUM_VOLUME) stateActions[i + k][j][0] = min(stateActions[i + k][j][0], addition);
+//						if (j + k <= SUM_VOLUME) stateActions[i][j + k][0] = min(stateActions[i][j + k][0], addition);
+//						//долить до деления
+//						if ((i - (-j + measure) >= 0) && (i - (-j + measure) <= SUM_VOLUME) && (j <= measure)) {
+//							stateActions[i - (-j + measure)][measure][k] = min(addition, stateActions[i - (-j + measure)][measure][k]);
+//						}
+//						if ((j - (-i + measure) >= 0) && (j - (-i + measure) <= SUM_VOLUME) && (i <= measure)) {
+//							stateActions[measure][j - (-i + measure)][k] = min(addition, stateActions[measure][j - (-i + measure)][k]);
+//						}
+//						if ((k - (-i + measure) >= 0) && (k - (-i + measure) <= SUM_VOLUME) && (i <= measure)) {
+//							stateActions[measure][j][k - (-i + measure)] = min(addition, stateActions[measure][j][k - (-i + measure)]);
+//						}
+//						if ((k - (-j + measure) >= 0) && (k - (-j + measure) <= SUM_VOLUME) && (j <= measure)) {
+//							stateActions[i][measure][k - (-j + measure)] = min(addition, stateActions[i][measure][k - (-j + measure)]);
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//	return findMinActions(stateActions, x);
+//}
+//
+//int main() {
+//	//high_resolution_clock::time_point t1 = high_resolution_clock::now();
+//	ios_base::sync_with_stdio(false);
+//	short x;
+//	ifstream in("in.txt");
+//	ofstream out("out.txt");
+//	State init;
+//	in >> x >> init.first >> init.second;
+//	init.third = SUM_VOLUME - init.first - init.second;
+//	vector<short> measures;
+//	short temp;
+//	while (in >> temp) {
+//		measures.push_back(temp);
+//	}
+//	uint32_t*** stateActions = new uint32_t**[MATRIX_DIM];
+//	for (uint16_t i = 0; i < MATRIX_DIM; ++i) {
+//		stateActions[i] = new uint32_t*[MATRIX_DIM];
+//		for (uint16_t j = 0; j < MATRIX_DIM; ++j) {
+//			stateActions[i][j] = new uint32_t[MATRIX_DIM];
+//			for (uint16_t k = 0; k < MATRIX_DIM; ++k) {
+//				stateActions[i][j][k] = UINT32_MAX;
+//			}
+//		}
+//	}
+//	stateActions[init.first][init.second][init.third] = 0;
+//	measures.pop_back();
+//	uint32_t result = countActions(stateActions, init, measures, x);
+//	if (result < UINT32_MAX) {
+//		out << result << endl;
+//	}
+//	else {
+//		out << "No solution" << endl;
+//	}
+//	//cout << duration_cast<microseconds>(high_resolution_clock::now() - t1).count();
+//	//system("pause");
+//	return 0;
+//}
