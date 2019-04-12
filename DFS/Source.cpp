c4950a95b99381a0d6715b9cfed46a55d36f8d45
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2019-04-12 22:35:08 +0300

    Added DFS (not working currently)

--- DFS/Source.cpp
+++ DFS/Source.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <stack>
 
 using namespace std;
 
@@ -15,4 +16,40 @@ int main() {
 			in >> adjMatrix[i][j];
 		}
 	}
+	uint32_t count = 1;
+	uint32_t u;
+	stack<uint32_t> st;
+	uint16_t* used = new uint16_t[n]{ 0 };
+	uint32_t* labels = new uint32_t[n];
+	uint32_t minUnused = 0;
+	while (minUnused != -1) {
+		st.push(minUnused + 1);
+		used[minUnused] = 1;
+		while (!st.empty()) {
+			u = st.top();
+			st.pop();
+			labels[u - 1] = count++;
+			for (int j = n - 1; j >= 0; --j) {
+				if (!used[j] && adjMatrix[u - 1][j]) {
+					st.push(j + 1);
+					used[j] = 1;
+				}
+			}
+		}
+		minUnused = -1;
+		for (size_t i = 0; i < n; ++i) {
+			if (!used[i]) {
+				minUnused = i;
+				break;
+			}
+		}
+	}
+	for (uint32_t i = 0; i < n; ++i) {
+		out << labels[i];
+		if (i != n - 1) {
+			out << ' ';
+		}
+	}
+	out << endl;
+	return 0;
 }
