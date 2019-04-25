d6f7e1094fb59049dc07283d87a8f8c90dc61758
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2019-04-25 18:53:45 +0300

    Some initial thoughts on Maths problem

--- Maths/Source.cpp
+++ Maths/Source.cpp
@@ -3,8 +3,83 @@
 #include <ctime>
 #include <algorithm>
 #include <vector>
+#include <stack>
 
 using namespace std;
+
+typedef pair<short, short> index;
+
+struct BinHeap {
+
+	index* heap;
+	size_t n;
+	size_t max_n;
+
+	BinHeap(size_t max = 12) {
+		max_n = max;
+		heap = new index[max_n];
+		n = 0;
+	}
+
+	void add(index key) {
+		if (n + 1 == max_n) {
+			max_n *= 2;
+			index* temp = new index[max_n];
+			for (size_t i = 0; i < n; ++i) {
+				temp[i] = heap[i];
+			}
+			delete[] heap;
+			heap = temp;
+		}
+		heap[++n] = key;
+		size_t pos = n;
+		while (heap[pos / 2].first < heap[pos].first) {
+			swap(heap[pos / 2], heap[pos]);
+			pos /= 2;
+		}
+		if (heap[pos / 2].first == heap[pos].first) {
+			while (heap[pos / 2].second > heap[pos].second) {
+				swap(heap[pos / 2], heap[pos]);
+				pos /= 2;
+			}
+		}
+	}
+
+	index get() {
+		return heap[0];
+	}
+
+	void remove() {
+		heap[0] = heap[n - 1];
+		--n;
+		size_t pos = 0;
+		while (heap[2 * pos + 1].first > heap[pos].first) {
+			if (heap[2 * pos + 1].first > heap[2 * pos].first) {
+				swap(heap[2 * pos + 1], heap[pos]);
+				pos = 2 * pos + 1;
+			}
+			else {
+				swap(heap[2 * pos], heap[pos]);
+				pos *= 2;
+			}
+		}
+		if (heap[pos / 2].first == heap[pos].first) {
+			while (heap[pos / 2].second < heap[pos].second) {
+				swap(heap[pos / 2], heap[pos]);
+				pos /= 2;
+			}
+		}
+	}
+
+	size_t find(index key) {
+		
+	}
+
+	bool isEmpty() {
+		return n == 0;
+	}
+
+};
 
 pair<vector<int>, vector<int>> test(size_t n) {
 	srand(time(NULL));
@@ -25,8 +100,55 @@ pair<vector<int>, vector<int>> test(size_t n) {
 	return make_pair(input, sums);
 }
 
-vector<int> findInput(vector<int>& sums) {
-	vector<int> result;
+vector<int> findInput(vector<int>& sums, size_t n) {
+	vector<int> result(n);
+	result[0] = sums[0] / 2;
+	if (n > 1) {
+		result[1] = sums[1] - result[0];
+	}
+	size_t count = 1;
+	size_t i = 3;
+	index cur;
+	/*BinHeap bh;
+	bh.add(make_pair(2, 2));
+	bh.add(make_pair(1, 3));*/
+	/*stack<index> profile;
+	stack<index> profileCopy;
+	profile.push(make_pair(1, 3));
+	profile.push(make_pair(2, 2));*/
+	while (i < n*n) {
+		/*do {
+			cur = bh.get();
+			bh.remove();
+		} while (!bh.isEmpty() && result[cur.first - 1] + result[cur.second - 1] != sums[i]);*/
+		/*do {
+			cur = profile.top();
+			profile.pop();
+			profileCopy.push(cur);
+		} while (!profile.empty() && result[cur.first - 1] + result[cur.second - 1] != sums[i]);
+		if (cur.first != cur.second) {
+			result[cur.second - 1] = sums[i] - result[cur.first - 1];
+			if (cur.second + 1 <= n) {
+				profile.push(make_pair(cur.first, cur.second + 1));
+			}
+			if (cur.first + 1 <= n) {
+				profile.push(make_pair(cur.first + 1, cur.second));
+			}
+			i += 2;
+		}
+		else {
+			if (cur.second + 1 <= n) {
+				profile.push(make_pair(cur.first, cur.second + 1));
+				profile.push(make_pair(cur.second + 1, cur.second + 1));
+			}
+			++i;
+		}
+		while (!profileCopy.empty()) {
+			cur = profileCopy.top();
+			profileCopy.pop();
+			profile.push(cur);
+		}*/
+	}
 	return result;
 }
 
@@ -48,18 +170,23 @@ int main() {
 	ofstream out("output.txt");
 	size_t n;
 	in >> n;
+	/*vector<int> sums(n * n);
+	for (size_t i = 0; i < n * n; ++i) {
+		in >> sums[i];
+	}
+	auto result = findInput(sums, n);*/
 
 
 	auto testData = test(n);
 	auto input = testData.first;
 	auto sums = testData.second;
-	auto result = findInput(sums);
+	auto result = findInput(sums, n);
 	size_t count = 0;
 	while (checkAnswer(input, result)) {
 		testData = test(n);
 		input = testData.first;
 		sums = testData.second;
-		result = findInput(sums);
+		result = findInput(sums, n);
 		cout << ++count << endl;
 	}
 	cout << "Input:\n";
