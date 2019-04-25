61de43ff6689dbf2d7c6c664bd6f8072794db3b0
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2019-04-25 20:58:49 +0300

    Fixed DFS

--- DFS/Source.cpp
+++ DFS/Source.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <stack>
+#include <vector>
 
 using namespace std;
 
@@ -9,47 +10,44 @@ int main() {
 	ofstream out("output.txt");
 	uint32_t n;
 	in >> n;
-	uint32_t** adjMatrix = new uint32_t*[n];
-	for (uint32_t i = 0; i < n; ++i) {
-		adjMatrix[i] = new uint32_t[n];
-		for (size_t j = 0; j < n; ++j) {
+	uint32_t** adjMatrix = new uint32_t*[n + 1];
+	for (uint32_t i = 1; i < n + 1; ++i) {
+		adjMatrix[i] = new uint32_t[n + 1];
+		for (size_t j = 1; j < n + 1; ++j) {
 			in >> adjMatrix[i][j];
 		}
 	}
 	uint32_t count = 1;
 	uint32_t u;
 	stack<uint32_t> st;
-	uint16_t* used = new uint16_t[n]{ 0 };
-	uint32_t* labels = new uint32_t[n];
-	uint32_t minUnused = 0;
-	while (minUnused != -1) {
-		st.push(minUnused + 1);
-		used[minUnused] = 1;
+	vector<bool> used(n + 1, false);
+	vector<uint32_t> labels(n + 1);
+	for (uint32_t minUnused = 1; minUnused < n + 1; ++minUnused) {
+		st.push(minUnused);
 		while (!st.empty()) {
 			u = st.top();
 			st.pop();
-			labels[u - 1] = count++;
-			for (int j = n - 1; j >= 0; --j) {
-				if (!used[j] && adjMatrix[u - 1][j]) {
-					st.push(j + 1);
-					used[j] = 1;
+			if (!used[u]) {
+				labels[u] = count++;
+				for (int j = n; j >= 1; --j) {
+					if (!used[j] && adjMatrix[u][j]) {
+						st.push(j);
+					}
 				}
 			}
-		}
-		minUnused = -1;
-		for (size_t i = 0; i < n; ++i) {
-			if (!used[i]) {
-				minUnused = i;
-				break;
-			}
+			used[u] = 1;
 		}
 	}
-	for (uint32_t i = 0; i < n; ++i) {
+	for (uint32_t i = 1; i < n + 1; ++i) {
 		out << labels[i];
-		if (i != n - 1) {
+		if (i != n) {
 			out << ' ';
 		}
 	}
+	for (uint32_t i = 1; i < n + 1; ++i) {
+		delete[] adjMatrix[i];
+	}
+	delete[] adjMatrix;
 	out << endl;
 	return 0;
 }
