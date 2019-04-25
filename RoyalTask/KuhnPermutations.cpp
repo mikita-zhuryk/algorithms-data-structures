d7180a0da4d6973342929eeffe46376241e29f19
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2019-04-25 20:59:32 +0300

    Simplified Kosaraju and fixed intersections

--- RoyalTask/KuhnPermutations.cpp
+++ RoyalTask/KuhnPermutations.cpp
@@ -4,31 +4,29 @@
 
 using namespace std;
 
-typedef pair<int, short> edge;
 
-
-void dfsOrder(vector<vector<edge>>& adjList, int v, vector<bool>& visited, vector<int>& order) {
+void dfsOrder(vector<vector<int>>& adjList, int v, vector<bool>& visited, vector<int>& order) {
 	visited[v] = true;
-	for (edge uv : adjList[v]) {
-		if ((uv.second == 1) && !visited[uv.first]) {
-			dfsOrder(adjList, uv.first, visited, order);
+	for (int u : adjList[v]) {
+		if (!visited[u]) {
+			dfsOrder(adjList, u, visited, order);
 		}
 	}
 	order.push_back(v);
 }
 
-void dfsComp(vector<vector<edge>>& adjList, int v, vector<bool>& visited, vector<int>& comp) {
+void dfsComp(vector<vector<int>>& adjList, int v, vector<bool>& visited, vector<int>& comp) {
 	visited[v] = true;
 	comp.push_back(v);
-	for (edge uv : adjList[v]) {
-		if ((uv.second == 1) && !visited[uv.first]) {
-			dfsComp(adjList, uv.first, visited, comp);
+	for (int u : adjList[v]) {
+		if (!visited[u]) {
+			dfsComp(adjList, u, visited, comp);
 		}
 	}
 }
 
-vector<vector<int>> kosaraju(vector<vector<edge>>& adjList, vector<int>& matching) {
-	vector<vector<edge>> inverseAdjList(adjList.size());
+vector<vector<int>> kosaraju(vector<vector<int>>& adjList) {
+	vector<vector<int>> inverseAdjList(adjList.size());
 	vector<bool> visited(adjList.size());
 	for (size_t i = 0; i < visited.size(); ++i) {
 		visited[i] = false;
@@ -36,7 +34,7 @@ vector<vector<int>> kosaraju(vector<vector<edge>>& adjList, vector<int>& matchin
 	vector<int> order;
 	for (size_t i = 0; i < adjList.size(); ++i) {
 		for (size_t j = 0; j < adjList[i].size(); ++j) {
-			inverseAdjList[adjList[i][j].first].push_back(make_pair(i, adjList[i][j].second));
+			inverseAdjList[adjList[i][j]].push_back(i);
 		}
 	}
 	for (size_t i = 0; i < adjList.size() / 2; ++i) {
@@ -49,13 +47,36 @@ vector<vector<int>> kosaraju(vector<vector<edge>>& adjList, vector<int>& matchin
 	}
 	vector<int> comp;
 	vector<vector<int>> components;
-	for (size_t i = 0; i < adjList.size() / 2; ++i) {
-		dfsComp(adjList, i, visited, comp);
-		components.push_back(comp);
-		comp.clear();
-		comp.resize(0);
+	for (int i = order.size() - 1; i >= 0; --i) {
+		if (!visited[order[i]]) {
+			dfsComp(adjList, order[i], visited, comp);
+			components.push_back(comp);
+			comp.clear();
+		}
 	}
 	return components;
+}
+
+vector<int> intersect(vector<int>& v1, vector<int>& v2) {
+	sort(v1.begin(), v1.end());
+	sort(v2.begin(), v2.end());
+	vector<int> result;
+	size_t i = 0;
+	size_t j = 0;
+	while ((i < v1.size()) && (j < v2.size())) {
+		if (v1[i] == v2[j]) {
+			result.push_back(v1[i]);
+			++i;
+			++j;
+		}
+		else if (v1[i] < v2[j]) {
+			++i;
+		}
+		else {
+			++j;
+		}
+	}
+	return result;
 }
 
 int main() {
@@ -65,59 +86,62 @@ int main() {
 	int n;
 	int m;
 	in >> n;
-	vector<vector<edge>> adj(2 * n);
+	vector<vector<int>> adj(2 * n);
 	int v;
 	int l;
 	for (int u = 0; u < n; ++u) {
 		in >> l;
 		for (int i = 0; i < l; ++i) {
 			in >> v;
-			adj[u].push_back(make_pair(n + v - 1, 1));
-			adj[n + v - 1].push_back(make_pair(u, -1));
+			adj[u].push_back(n + v - 1);
 		}
 	}
+	vector<vector<int>> adjCopy = adj;
 	vector<int> matching(n);
 	for (int i = 0; i < n; ++i) {
 		in >> matching[i];
 		matching[i] += n - 1;
-		for (edge& uv : adj[i]) {
-			if (uv.first == matching[i]) {
-				uv.second = -uv.second;
-				for (edge& vu : adj[uv.first]) {
-					if (vu.first == i) {
-						vu.second = -vu.second;
-					}
-				}
-			}
-		}
-	}
-	auto components = kosaraju(adj, matching);
-	size_t j = 0;
-	for (size_t i = 0; i < components.size(); ++i) {
-		j = 0;
-		sort(components[i].begin(), components[i].end());
-		while (components[i][j] < n) {
-			++j;
-			if (j == components[i].size()) {
+		adj[matching[i]].push_back(i);
+		for (auto it = adj[i].begin(); it != adj[i].end(); ++it) {
+			if (*it == matching[i]) {
+				swap(*it, adj[i][adj[i].size() - 1]);
+				adj[i].resize(adj[i].size() - 1);
 				break;
 			}
 		}
-		if (j == 1) {
+	}
+	auto components = kosaraju(adj);
+	vector<int> table(n);
+	for (size_t i = 0; i < n; ++i) {
+		table[i] = -1;
+	}
+	for (size_t i = 0; i < components.size(); ++i) {
+		for (size_t j = 0; j < components[i].size(); ++j) {
+			if ((components[i][j] < n) && (table[components[i][j]] == -1) && (components[i].size() > 1)) {
+				table[components[i][j]] = i;
+			}
+		}
+	}
+	vector<int> inter;
+	size_t lessThanN;
+	for (size_t i = 0; i < n; ++i) {
+		lessThanN = 0;
+		if (table[i] != -1) {
+			for (size_t j = 0; j < components[table[i]].size(); ++j) {
+				if (components[table[i]][j] < n) {
+					++lessThanN;
+				}
+			}
+		}
+		if ((table[i] == -1) || (lessThanN == 1)) {
 			out << 1 << ' ' << matching[i] - n + 1 << endl;
 		}
 		else {
-			for (size_t k = 1; k < j; ++k) {
-				if (components[i][k] > i) {
-					components[components[i][k]].clear();
-					for (size_t s = 0; s < components[i].size(); ++s) {
-						components[components[i][k]].push_back(components[i][s]);
-					}
-				}
-			}
-			out << components[i].size() - j << ' ';
-			for (size_t k = j; k < components[i].size(); ++k) {
-				out << components[i][k] - n + 1;
-				if (k != components[i].size() - 1) {
+			inter = intersect(components[table[i]], adjCopy[i]);
+			out << inter.size() << ' ';
+			for (size_t j = 0; j < inter.size(); ++j) {
+				out << inter[j] - n + 1;
+				if (j != inter.size() - 1) {
 					out << ' ';
 				}
 			}
