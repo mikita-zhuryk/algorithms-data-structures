1d18df36b5a0eaded229a1cd56408945e681725e
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2019-05-23 23:26:18 +0300

    Fixed Dijkstra

--- Dijkstra/PrQueueDijkstra.cpp
+++ Dijkstra/PrQueueDijkstra.cpp
@@ -1,43 +1,50 @@
 #include <fstream>
 #include <queue>
+#pragma warning(disable: 4996)
 
 using namespace std;
 
 typedef pair<uint32_t, uint32_t> edge;
 
+typedef pair<uint32_t, int64_t> vertex;
+
 int main() {
-	ios_base::sync_with_stdio(false);
-	ifstream in("input.txt");
-	ofstream out("output.txt");
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 	uint32_t n;
 	uint32_t m;
-	in >> n >> m;
-	vector<vector<edge>> adj(n);
+	scanf("%d %d", &n, &m);
+	vector<vector<edge>> adj(n + 1);
 	uint32_t u;
 	uint32_t v;
 	uint32_t w;
+	uint32_t j;
+	bool found;
 	for (uint32_t i = 0; i < m; ++i) {
-		in >> u >> v >> w;
-		adj[u - 1].push_back(edge(v, w));
+		scanf("%d %d %d", &u, &v, &w);
+		found = false;
+		if (u != v) {
+			adj[u].push_back(edge(v, w));
+			adj[v].push_back(edge(u, w));
+		}
 	}
-	uint64_t* paths = new uint64_t[n];
-	for (uint32_t i = 0; i < n; ++i) {
-		paths[i] = INT_MAX;
-	}
-	auto comp = [&paths](uint32_t a, uint32_t b) { return paths[a - 1] < paths[b - 1]; };
-	priority_queue<uint32_t, vector<uint32_t>, decltype(comp)> q(comp, vector<uint32_t>(0));
-	paths[0] = 0;
-	q.push(1);
+	vector<int64_t> d(n + 1, INT64_MAX);
+	auto comp = [](vertex u, vertex v) { return u.second > v.second; };
+	priority_queue<vertex, vector<vertex>, decltype(comp)> q(comp, vector<vertex>(0));
+	size_t size;
+	q.push({ 1, 0 });
+	vertex cur;
 	while (!q.empty()) {
-		u = q.top();
+		cur = q.top();
 		q.pop();
-		for (auto v : adj[u - 1]) {
-			if (paths[v.first - 1] > paths[u - 1] + v.second) {
-				paths[v.first - 1] = paths[u - 1] + v.second;
-				q.push(v.first);
+		if (d[cur.first] > cur.second) {
+			d[cur.first] = cur.second;
+			size = adj[cur.first].size();
+			for (size_t i = 0; i < size; ++i) {
+				q.push({ adj[cur.first][i].first, min(d[cur.first] + adj[cur.first][i].second, d[adj[cur.first][i].first]) });
 			}
 		}
 	}
-	out << paths[n - 1] << endl;
+	printf("%I64d\n", d[n]);
 	return 0;
 }
